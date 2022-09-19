25b841ff1f956b8d8fe486df6d9cd17dd89e4e0b
Gopalakrishnan Subramani <gopalinlatvia@gmail.com>
2022-09-19 09:12:46 +0300

    updating files

--- src/utils/data_mgmt.py
+++ src/utils/data_mgmt.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 def get_data(validation_datasize):
     mnist = tf.keras.datasets.mnist
-    (X_train_full, y_train_full), (X_test, y_test) = mnist.load_data(path="https://storage.googleapis.com/tensorflow/tf-keras-datasets/train-labels-idx1-ubyte.gz")
+    (X_train_full, y_train_full), (X_test, y_test) = mnist.load_data(path="/media/krish/2492a8f6-b494-45ae-8418-bceed4c56914/home/krishgopal/Downloads/mnist.npz")
     X_valid, X_train = X_train_full[:validation_datasize] / 255.0, X_train_full[validation_datasize:] / 255.0
     y_valid, y_train = y_train_full[:validation_datasize], y_train_full[validation_datasize:]
     X_test = X_test / 255.0
